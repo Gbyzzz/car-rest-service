b304fbbee166db5ecf1a58a644f50d3d4065dc43
Anton Pinchuk <gbyzzz@gmail.com>
2023-06-30 14:16:28 +0300

    deleted spring security

--- src/main/java/ua/foxminded/pinchuk/javaspring/carrestservice/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package ua.foxminded.pinchuk.javaspring.carrestservice.service;
-
-import org.springframework.http.ResponseEntity;
-import ua.foxminded.pinchuk.javaspring.carrestservice.controller.payload.request.LoginRequest;
-
-public interface AuthenticationService {
-    ResponseEntity login(LoginRequest request) throws Exception;
-}
