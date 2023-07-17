85bf5248bfc41f728a1b84703c173b85636698f9
Anton Pinchuk <gbyzzz@gmail.com>
2023-07-18 00:32:27 +0300

    sime minor fixes

--- src/main/java/ua/foxminded/pinchuk/javaspring/carrestservice/security/SecurityConfig.java
+++ src/main/java/ua/foxminded/pinchuk/javaspring/carrestservice/security/SecurityConfig.java
@@ -5,25 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -65,9 +58,11 @@ public class SecurityConfig {
                 Collection<GrantedAuthority> authorities = new ArrayList<>();
                 List<String> permissions = jwt.getClaimAsStringList("permissions");
                 if (permissions != null) {
-                    permissions.forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission)));
+                    permissions.forEach(permission ->
+                            authorities.add(new SimpleGrantedAuthority(permission)));
                 }
-                return new UsernamePasswordAuthenticationToken(jwt.getSubject(), jwt.getTokenValue(), authorities);
+                return new UsernamePasswordAuthenticationToken(jwt.getSubject(),
+                        jwt.getTokenValue(), authorities);
             }
         };
     }
@@ -77,7 +72,7 @@ public class SecurityConfig {
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
         OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAudience, withIssuer);
 
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
     }
